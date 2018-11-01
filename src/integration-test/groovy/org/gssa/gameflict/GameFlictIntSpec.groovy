41ace3e2d319c088933c6c2c21db181b2e832b8b
niravassar <assarn@objectcomputing.com>
2018-11-01 18:48:27 -0500

    change game test

--- src/integration-test/groovy/org/gssa/gameflict/GameFlictIntSpec.groovy
+++ src/integration-test/groovy/org/gssa/gameflict/GameFlictIntSpec.groovy
@@ -32,6 +32,32 @@ class GameFlictIntSpec extends Specification {
         List<Game> games = Game.list()
 
         then:
-        games.size() == 7
+        games.size() == 12
+    }
+
+    void "test one game is updated"() {
+        when:
+        String gssaRec = "GSSA Rec Fall 2018"
+        File gameCsv1 = new File("$SAMPLE_XLS_PATH/GSSARECF186_sample.csv")
+        gameFlictService.importAndSaveGames(gameCsv1.path, gssaRec)
+        List<Game> games = Game.list()
+
+        then:
+        games.size() == 6
+        games[0].gameNumber == 935
+        games[0].date.toString() == "2018-10-27"
+        games[0].time.toString() == "09:00"
+
+        when:
+        File gameCsv2 = new File("$SAMPLE_XLS_PATH/GSSARECF186_sample_game_moved.csv")
+        gameFlictService.importAndSaveGames(gameCsv2.path, gssaRec)
+        games = Game.list()
+
+        then:
+        games.size() == 6
+        games[0].gameNumber == 935
+        games[0].date.toString() == "2018-12-01"
+        games[0].time.toString() == "17:00"
+
     }
 }
